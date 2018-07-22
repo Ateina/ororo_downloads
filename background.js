67ff4e07530cbcb5c90694353e13ecb61778207a
Ateina <totora246@gmail.com>
2018-07-22 23:30:58 +0300

    added toolbar bar action;

---
+++ background.js
@@ -0,0 +1,19 @@
+function onError(error) {
+    console.error(`Error: ${error}`);
+}
+
+function sendMessageToTabs(tabs) {
+    for (let tab of tabs) {
+        browser.tabs.sendMessage(
+            tab.id,
+            { message: "download" }
+        )
+    }
+}
+
+browser.browserAction.onClicked.addListener(() => {
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    }).then(sendMessageToTabs).catch(onError);
+});
--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var elem = document.getElementById("3");
-var chapters = elem.getElementsByClassName("js-media-download");
-var firstChapter = chapters[0];
-firstChapter.click();
+function downloadSeries() {
+    var elem = document.getElementById("3");
+    var chapters = elem.getElementsByClassName("js-media-download");
+    var firstChapter = chapters[0];
+    firstChapter.click();
+}
+
+browser.runtime.onMessage.addListener(() => {
+    downloadSeries();
+});
