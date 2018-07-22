36265b9e7c2a1907f948d6b49cff5894415ffc31
Ateina <totora246@gmail.com>
2018-07-23 01:01:03 +0300

    update script.js with finding cuurent season;

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function downloadSeries() {
-    var elem = document.getElementById("3");
+    var seasonNumber = getSeasonNumber();
+    var chapters = gettAllChapters(seasonNumber);
+    chapters = Array.from(chapters);
+    chapters[0].click();
+}
+
+function getSeasonNumber() {
+    var season = document.getElementsByClassName("js-season-link-wrapper active");
+    var s = season[0].getElementsByClassName('js-season-link');
+    var seasonNumber = s[0].hash.replace('#', '');
+    console.log(seasonNumber);
+    return seasonNumber;
+}
+
+function gettAllChapters(seasonNumber) {
+    var elem = document.getElementById(seasonNumber);
     var chapters = elem.getElementsByClassName("js-media-download");
-    var firstChapter = chapters[0];
-    firstChapter.click();
+    return chapters;
 }
 
 browser.runtime.onMessage.addListener(() => {
